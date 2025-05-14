6030b5dc2ac18bd36f1243be02f26c10166a53b4
Apologialinux <70620891+apologialinux@users.noreply.github.com>
2025-05-14 05:03:33 -0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (this.checked) {
             document.body.style.zoom = '1.0';
         } else {
-            document.body.style.zoom = '0.1';
+            document.body.style.zoom = '0.2';
         }
     });
 });
